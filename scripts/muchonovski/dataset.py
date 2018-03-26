ec4e896d382abfe5ddbed465baf20d29edb6a2be
sugyan <sugi1982@gmail.com>
2018-03-27 01:06:42 +0900

    Add muchonovski dataset generator

--- scripts/muchonovski/dataset.py
+++ scripts/muchonovski/dataset.py
@@ -1,17 +1,66 @@
 import os
+import random
 from PIL import Image
+
+IMAGE_SIZE = 48
 
 
 class Generator:
     def __init__(self, imageDir, dataDir):
         self.imageDir = imageDir
         self.dataDir = dataDir
+        self.bans = ['dirty', 'gohan', 'kaya_a', 'kaya_b', 'kaya_c', 'kaya_d', 'muji', 'oritatami', 'paper', 'stripe']
+        self.masus = ['dot_xy', 'dot', 'handwriting', 'nodot_xy', 'nodot']
+        self.komas = ['dirty', 'kinki', 'kinki_torafu', 'ryoko', 'ryoko_torafu']
+        self.pieces = [
+            'Gfu', 'Gkyo',  'Gkei',  'Ggin',  'Gkaku', 'Ghi',
+            'Gto', 'Gnkyo', 'Gnkei', 'Gngin', 'Guma',  'Gryu', 'Gkin', 'Gou',
+            'Sfu', 'Skyo',  'Skei',  'Sgin',  'Skaku', 'Shi',
+            'Sto', 'Snkyo', 'Snkei', 'Sngin', 'Suma',  'Sryu', 'Skin', 'Sou']
+        self.pieceMap = {
+            'Gfu':   'W_FU', 'Sfu':   'B_FU',
+            'Gto':   'W_TO', 'Sto':   'B_TO',
+            'Gkyo':  'W_KY', 'Skyo':  'B_KY',
+            'Gnkyo': 'W_NY', 'Snkyo': 'B_NY',
+            'Gkei':  'W_KE', 'Skei':  'B_KE',
+            'Gnkei': 'W_NK', 'Snkei': 'B_NK',
+            'Ggin':  'W_GI', 'Sgin':  'B_GI',
+            'Gngin': 'W_NG', 'Sngin': 'B_NG',
+            'Gkin':  'W_KI', 'Skin':  'B_KI',
+            'Gkaku': 'W_KA', 'Skaku': 'B_KA',
+            'Guma':  'W_UM', 'Suma':  'B_UM',
+            'Ghi':   'W_HI', 'Shi':   'B_HI',
+            'Gryu':  'W_RY', 'Sryu':  'B_RY',
+            'Gou':   'W_OU', 'Sou':   'B_OU',
+        }
 
     def run(self):
-        img = Image.open(os.path.join(self.imageDir, 'koma', 'koma_dirty', 'Gfu.png'))
-        savePath = os.path.join(self.dataDir, 'W_FU', '1.jpg')
-        with open(savePath, 'w') as fp:
-            img.convert('RGB').save(fp)
+        for piece in self.pieces:
+            saveDir = os.path.join(self.dataDir, self.pieceMap[piece])
+            for i in range(10):
+                ban = random.choice(self.bans)
+                masu = random.choice(self.masus)
+                koma = random.choice(self.komas)
+                img = self.generate(ban, masu, koma, piece)
+                filename = 'muchonovski_{:02d}.jpg'.format(i)
+                savePath = os.path.join(saveDir, filename)
+                print('{}: {}...'.format(os.path.basename(saveDir), filename))
+                with open(savePath, 'w') as fp:
+                    img.convert('RGB').save(fp, quality=90)
+
+    def generate(self, banName, masuName, komaName, piece):
+        ban = Image.open(os.path.join(self.imageDir, 'ban', 'ban_{}.png'.format(banName)))
+        masu = Image.open(os.path.join(self.imageDir, 'masu', 'masu_{}.png'.format(masuName)))
+        koma = Image.open(os.path.join(self.imageDir, 'koma', 'koma_{}'.format(komaName), '{}.png'.format(piece)))
+        file, rank = random.randrange(9), random.randrange(9)
+        offset = (11 + 43 * file, 11 + 48 * rank)
+        ban.alpha_composite(masu)
+        ban.alpha_composite(koma, dest=offset)
+        return ban.crop(box=(
+            offset[0] - int((IMAGE_SIZE - 43) / 2),
+            offset[1] - int((IMAGE_SIZE - 48) / 2),
+            offset[0] - int((IMAGE_SIZE - 43) / 2) + IMAGE_SIZE,
+            offset[1] - int((IMAGE_SIZE - 48) / 2) + IMAGE_SIZE))
 
 
 if __name__ == '__main__':
