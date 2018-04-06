40fb92ec8a3fae03d31d5182bb2a5228f2e8c92b
sugyan <sugi1982@gmail.com>
2018-04-06 18:47:07 +0900

    Update convert.py

--- scripts/convert.py
+++ scripts/convert.py
@@ -4,5 +4,10 @@ tf_converter.convert(
     tf_model_path='output_graph.pb',
     mlmodel_path='Shogi.mlmodel',
     input_name_shape_dict={'input:0': [1, 128, 128, 3]},
+    output_feature_names=['final_result:0'],
     image_input_names=['input:0'],
-    output_feature_names=['final_result:0'])
+    class_labels='output_labels.txt',
+    red_bias=-1.0,
+    green_bias=-1.0,
+    blue_bias=-1.0,
+    image_scale=2.0/255.0)
