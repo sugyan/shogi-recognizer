7361d2afb270f8b4d917a892e593a44b2d924961
sugyan <sugi1982@gmail.com>
2018-04-21 19:40:53 +0900

    Update converter script

--- scripts/convert.py
+++ scripts/convert.py
@@ -3,9 +3,9 @@ import tfcoreml as tf_converter
 tf_converter.convert(
     tf_model_path='output_graph.pb',
     mlmodel_path='Shogi.mlmodel',
-    input_name_shape_dict={'input:0': [1, 128, 128, 3]},
+    input_name_shape_dict={'Placeholder:0': [1, 96, 96, 3]},
     output_feature_names=['final_result:0'],
-    image_input_names=['input:0'],
+    image_input_names=['Placeholder:0'],
     class_labels='output_labels.txt',
     red_bias=-1.0,
     green_bias=-1.0,
