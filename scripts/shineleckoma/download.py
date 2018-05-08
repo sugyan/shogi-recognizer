c6e6d5942aa03e9798beb09869b14854df5bb36d
sugyan <sugi1982@gmail.com>
2018-05-09 00:40:03 +0900

    Update shinelekoma downloader

--- scripts/shineleckoma/download.py
+++ scripts/shineleckoma/download.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 import os
 import urllib.request
-from bs4 import BeautifulSoup
 
 
 class Downloader:
@@ -12,19 +11,26 @@ class Downloader:
         self.dir = directory
 
     def run(self):
-        for path in ['page1.htm', 'page2.htm', 'page3.htm', 'page4.htm']:
-            self.downloadImages(path)
+        fileNames = [
+            'koma9.bmp',   'koma18.bmp',  'koma20.bmp',  'koma25.bmp',
+            'koma35.bmp',  'koma36.bmp',  'koma40.bmp',  'koma41.bmp',
+            'koma44.bmp',  'koma48.bmp',  'koma50.bmp',  'koma52.bmp',
+            'koma56.bmp',  'koma59.bmp',  'koma61.bmp',  'koma63.bmp',
+            'koma64.bmp',  'koma66.bmp',  'koma73.bmp',  'koma76.bmp',
+            'koma82.bmp',  'koma86.bmp',  'koma92.bmp',  'koma94.bmp',
+            'koma96.bmp',  'koma98.bmp',  'koma100.bmp', 'koma101.bmp',
+            'koma105.bmp', 'koma107.bmp', 'koma109.bmp', 'koma113.bmp',
+            'koma115.bmp', 'koma117.bmp', 'koma119.bmp', 'koma120.bmp',
+            'koma121.bmp',
+        ]
+        for fileName in fileNames:
+            self.downloadImages(fileName)
 
-    def downloadImages(self, path):
-        soup = BeautifulSoup(urllib.request.urlopen(Downloader.url + path), 'html.parser')
-        for img in soup.find_all('img'):
-            fileName = img['src']
-            if not fileName.endswith('.bmp'):
-                continue
-            print('download {}...'.format(fileName))
-            savePath = os.path.join(self.dir, fileName)
-            imageUrl = Downloader.url + fileName
-            urllib.request.urlretrieve(imageUrl, savePath)
+    def downloadImages(self, fileName):
+        print('download {}...'.format(fileName))
+        savePath = os.path.join(self.dir, fileName)
+        imageUrl = Downloader.url + fileName
+        urllib.request.urlretrieve(imageUrl, savePath)
 
 
 if __name__ == '__main__':
