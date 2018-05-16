379bcd96b0a16b8da60ceaf4653b61ccef124680
sugyan <sugi1982@gmail.com>
2018-05-17 00:23:45 +0900

    Update genrator scripts

--- scripts/dataset/generate.py
+++ scripts/dataset/generate.py
@@ -4,6 +4,7 @@ import os
 import random
 import re
 from generators import AlphaGenerator, ShineleckomaGenerator, CharacterGenerator
+from PIL import ImageEnhance
 
 PIECES = [
     'B_FU', 'W_FU',
@@ -31,15 +32,16 @@ def run(dataDir, g):
         weights.append(weight)
 
     for piece in PIECES:
-        for i in range(2000):
+        for i in range(3000):
             fileName = '{:04d}.jpg'.format(i)
             savePath = os.path.join(dataDir, piece, fileName)
             generator = random.choices(generators, weights=weights)[0]
             img = generator.generate(piece)
+            for e in [ImageEnhance.Color, ImageEnhance.Contrast, ImageEnhance.Brightness, ImageEnhance.Sharpness]:
+                img = e(img).enhance(random.normalvariate(1.0, 0.1))
             print('{}: generated {}...'.format(piece, fileName))
             with open(savePath, 'w') as fp:
                 img.save(fp, quality=random.randint(80, 100))
-        break
 
 
 if __name__ == '__main__':
@@ -49,11 +51,11 @@ if __name__ == '__main__':
     fontFilter = re.compile(r'W[6-9]')
     fonts = [f for f in args.fonts if not fontFilter.search(f)]
 
-    dataDir = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'dataset')
-    imageDir = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'images')
+    dataDir = os.path.join(os.path.dirname(__file__), '..', '..', 'dataset')
+    imageDir = os.path.join(os.path.dirname(__file__), '..', '..', 'images')
     generators = {
-        AlphaGenerator(imageDir): 5,
-        ShineleckomaGenerator(imageDir): 4,
-        CharacterGenerator(fonts): 1,
+        AlphaGenerator(imageDir): 7,
+        ShineleckomaGenerator(imageDir): 18,
+        CharacterGenerator(fonts): 3,
     }
     run(dataDir, generators)
