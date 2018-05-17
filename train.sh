215ec00355acd72331b5cb3ed537b4ab0c5232f0
sugyan <sugi1982@gmail.com>
2018-05-17 14:49:07 +0900

    Update

--- train.sh
+++ train.sh
@@ -4,7 +4,4 @@ BASEDIR=$(dirname "$0")
 python scripts/retrain.py \
     --image_dir ${BASEDIR}/dataset \
     --tfhub_module https://tfhub.dev/google/imagenet/mobilenet_v2_050_96/classification/1 \
-    --random_crop 3 \
-    --random_scale 3 \
-    --random_brightness 3 \
-    --how_many_training_steps 3000
+    --how_many_training_steps 30000
