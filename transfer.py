cb8fd685c361dc69806033cf184d188ef66d4fd8
sugyan <sugi1982@gmail.com>
2019-08-31 22:26:44 +0900

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -1,6 +1,8 @@
 import argparse
-import json
+import glob
 import os
+import pickle
+import numpy as np
 import tensorflow as tf
 
 IMAGE_SIZE = (96, 96)
@@ -28,99 +30,93 @@ def tfrecord_dataset(filepath):
     return tf.data.TFRecordDataset(filepath).map(parser)
 
 
-def dump_features(data_dir, filepath):
+def dump_features(data_dir, features_dir):
     model = mobilenet_v2()
     model.trainable = False
     model.build((None,) + IMAGE_SIZE + (3,))
     model.summary()
-    batch_size = 32
+    batch_size = 50
 
-    data = {
-        'training': [],
-        'validation': [],
-        'testing': []
-    }
-    print('calculate features of training data')
-    for images, labels in tfrecord_dataset(os.path.join(data_dir, 'training.tfrecord')).batch(batch_size):
-        for features, label in zip(model(images), labels):
-            data['training'].append([features.numpy().tolist(), int(label.numpy())])
-        print({k: len(v) for k, v in data.items()})
-    print('calculate features of validation data')
-    for images, labels in tfrecord_dataset(os.path.join(data_dir, 'validation.tfrecord')).batch(batch_size):
-        for features, label in zip(model(images), labels):
-            data['validation'].append([features.numpy().tolist(), int(label.numpy())])
-        print({k: len(v) for k, v in data.items()})
-    print('calculate features of testing data')
-    for images, labels in tfrecord_dataset(os.path.join(data_dir, 'testing.tfrecord')).batch(batch_size):
-        for features, label in zip(model(images), labels):
-            data['testing'].append([features.numpy().tolist(), int(label.numpy())])
-        print({k: len(v) for k, v in data.items()})
-    with open(filepath, 'w') as fp:
-        json.dump(data, fp)
-
-
-class FeaturesSequence(tf.keras.utils.Sequence):
-    def __init__(self, data):
-        self.data = data
-
-    def __getitem__(self, idx):
-        return tf.constant([self.data[idx][0]]), tf.constant([self.data[idx][1]])
-
-    def __len__(self):
-        return len(self.data)
-
-
-class Callback(tf.keras.callbacks.Callback):
-    def on_train_batch_begin(self, batch, logs=None):
-        # print(batch, logs)
-        pass
+    for filepath in glob.glob(os.path.join(data_dir, '*.tfrecord')):
+        category = os.path.splitext(os.path.basename(filepath))[0]
+        print(f'calculate features of {category} data', end='', flush=True)
+        inputs, targets = [], []
+        for images, labels in tfrecord_dataset(filepath).batch(batch_size):
+            for features, label in zip(model(images), labels):
+                inputs.append(features.numpy())
+                targets.append(label.numpy())
+            print('.', end='', flush=True)
+        print()
+        np.savez(os.path.join(features_dir, f'{category}.npz'), inputs=inputs, targets=targets)
 
 
 def run(args):
     with open(os.path.join(args.data_dir, 'labels.txt')) as fp:
         labels = [line.strip() for line in fp.readlines()]
 
-    if not os.path.exists(args.features):
-        dump_features(args.data_dir, args.features)
+    if len(glob.glob(os.path.join(args.features_dir, '*.npz'))) == 0:
+        os.makedirs(args.features_dir, exist_ok=True)
+        dump_features(args.data_dir, args.features_dir)
 
-    with open(args.features, 'r') as fp:
-        data = json.load(fp)
+    def dataset(category):
+        npz = np.load(os.path.join(args.features_dir, f'{category}.npz'))
+        inputs = npz['inputs']
+        targets = npz['targets']
+        size = inputs.shape[0]
+        return tf.data.Dataset.from_tensor_slices((inputs, targets)) \
+            .shuffle(inputs.shape[0]) \
+            .batch(args.batch_size), size
 
-    training_seq = FeaturesSequence(data['training'])
-    # validation_seq = FeaturesSequence(data['validation'])
+    training_data, training_size = dataset('training')
+    validation_data, _ = dataset('validation')
     model = tf.keras.Sequential([
-        tf.keras.layers.Dropout(rate=0.2),
-        tf.keras.layers.Dense(len(labels), activation='softmax',
-                              kernel_regularizer=tf.keras.regularizers.l2(0.0001))
+        tf.keras.layers.Dropout(rate=0.1),
+        tf.keras.layers.Dense(
+            len(labels), activation='softmax',
+            kernel_regularizer=tf.keras.regularizers.l2(0.0001)),
     ])
-    model.build((None, 1280,))
+    model.build((None, 1280))
+    model.summary()
     model.compile(
-        optimizer=tf.keras.optimizers.SGD(),
+        optimizer=tf.keras.optimizers.RMSprop(),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=['accuracy'])
-    model.fit(
-        training_seq,
-        epochs=3,
-        # validation_data=validation_seq,
-        callbacks=[Callback()])
+    history = model.fit(
+        training_data.repeat(),
+        epochs=50,
+        steps_per_epoch=training_size // args.batch_size,
+        validation_data=validation_data,
+        verbose=2)
+
+    testing_data, _ = dataset('testing')
+    test_result = model.evaluate(testing_data, verbose=0)
+    with open('results.pkl', 'wb') as fp:
+        pickle.dump({'history': history.history, 'result': test_result}, fp)
+
+    model.save_weights(os.path.join(args.weights_dir, 'transfer.h5'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--data_dir',
-        help='''Path to directories of tfrecord files''',
+        help='''Path to directory of tfrecord files''',
         type=str,
-        default=os.path.join(os.path.dirname(__file__), '..', 'data'))
+        default=os.path.join(os.path.dirname(__file__), 'data'))
     parser.add_argument(
-        '--features',
-        help='''Features file''',
+        '--features_dir',
+        help='''Path to directory of features files''',
         type=str,
-        default='features.json')
+        default='features')
+    parser.add_argument(
+        '--weights_dir',
+        help='''Path to directory of weights files''',
+        type=str,
+        default='weights')
     parser.add_argument(
         '--batch_size',
         help='''Batch size''',
         type=int,
-        default=64)
+        default=32)
     args = parser.parse_args()
     run(args)
