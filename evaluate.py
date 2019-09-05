1470dd7a9bf47f63df96b960a1a801b46b28854e
sugyan <sugi1982@gmail.com>
2019-09-05 23:57:08 +0900

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -2,25 +2,18 @@ import argparse
 import os
 import tensorflow as tf
 
-from model import build_model
 from dataset import tfrecord_dataset
 
 
-def evaluate(data_dir, weights_path):
-    with open(os.path.join(data_dir, 'labels.txt')) as fp:
-        labels = [line.strip() for line in fp.readlines()]
-
-    model = build_model(len(labels))
+def evaluate(data_dir, model_path):
+    model = tf.keras.models.load_model(model_path)
     model.summary()
-    model.load_weights(weights_path)
-    model.trainable = False
-
     model.compile(
         loss=tf.keras.losses.SparseCategoricalCrossentropy(),
         metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
     testing_data, testing_size = tfrecord_dataset(os.path.join(data_dir, 'testing.tfrecord'))
-    test_result = model.evaluate(testing_data.batch(1), steps=testing_size)
+    test_result = model.evaluate(testing_data.shuffle(testing_size).batch(1), steps=testing_size)
     print(test_result)
 
 
@@ -32,9 +25,9 @@ if __name__ == '__main__':
         type=str,
         default=os.path.join(os.path.dirname(__file__), 'data'))
     parser.add_argument(
-        '--weights_path',
-        help='''Path to file of trained weights''',
+        '--model_path',
+        help='''Path to file of trained models''',
         type=str,
         default='')
     args = parser.parse_args()
-    evaluate(args.data_dir, args.weights_path)
+    evaluate(args.data_dir, args.model_path)
